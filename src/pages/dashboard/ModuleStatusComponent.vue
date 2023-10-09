f6d1c56d48ceba0974e81c57b75b7d6075ab46eb
Mykola Zhyhallo <mzhyhall@gmail.com>
2023-10-09 22:29:44 +0200

    Dynamic module info on dashboard page

--- src/pages/dashboard/ModuleStatusComponent.vue
+++ src/pages/dashboard/ModuleStatusComponent.vue
@@ -3,7 +3,36 @@
         <div class="col bg-yellow-7" style="max-width: 62px; height: 62px;"> <q-icon name="fa-solid fa-layer-group" size="md" class="q-pt-md q-pl-md"></q-icon></div>
         <div class="col q-pl-md q-pt-md">
             <div class="text-caption text-uppercase text-bold text-grey">Module Status</div>
-            <div class="text-subtitle1 text-bold">RUNNING | DB1000N</div>
+            <div class="text-subtitle1 text-bold">{{ displayMessage }}</div>
         </div>
     </q-card>
 </template>
+
+<script lang="ts" setup>
+import { computed, onMounted, ref } from 'vue'
+import { ModuleName } from 'app/lib/module/module';
+
+const selectedModule = ref("DB1000N" as ModuleName | null)
+const moduleEnabled = ref(false)
+
+const displayMessage = computed(()=>{
+    if (selectedModule.value == null) {
+        return "NOT CONFIGURED"
+    }
+    if (moduleEnabled.value) {
+        return "RUNNING | " + selectedModule.value
+    }
+    return "IDLE | " + selectedModule.value
+})
+
+async function loadState() {
+    const executionEngineState = await window.executionEngineAPI.getState()
+    moduleEnabled.value = executionEngineState.run
+    selectedModule.value = executionEngineState.moduleToRun || null
+}
+
+onMounted(async () => {
+    await loadState()
+})
+
+</script>
